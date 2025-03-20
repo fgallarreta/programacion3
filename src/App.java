3c71d6b48e6d0e2c125e0f60793630d238a068a4
Facundo Gallarreta <facundo.gallarreta1999@gmail.com>
2025-03-20 14:33:29 -0300

    Ejercicio1

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        MySimpleLinkedList<Integer> miLista = new MySimpleLinkedList<>();
+        miLista.insertFront(1);
+        miLista.insertFront(2);
+        miLista.insertFront(5);
+        miLista.insertFront(3);
+
+    }
+}
---
+++ src/MySimpleLinkedList.java
@@ -0,0 +1,65 @@
+public class MySimpleLinkedList<T> {
+
+    private Node<T> first;
+    private int size;
+
+    public MySimpleLinkedList() {
+        this.first = null;
+        this.size = 0;
+    }
+
+    public void insertFront(T info) {
+        Node<T> tmp = new Node<T>(info, null);
+        tmp.setNext(this.first);
+        this.first = tmp;
+        this.size++;
+    }
+
+    public T extractFront() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        T prim = this.first.getInfo();
+        this.first = this.first.getNext();
+        this.size--;
+        return prim;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public T get(int index) {
+        int i = 0;
+        Node<T> aux = this.first;
+        if (index < this.size) {
+
+            while (i < index) {
+                if (index == 0) {
+                    return aux.getInfo();
+                } else {
+                    aux = aux.getNext();
+                }
+                i++;
+            }
+        } else {
+            return null;
+        }
+
+        return aux.getInfo();
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public String toString() {
+        String aux = "";
+        for (int i = 0; i < this.size; i++) {
+            aux += " " + this.get(i);
+        }
+        return aux;
+    }
+
+}
